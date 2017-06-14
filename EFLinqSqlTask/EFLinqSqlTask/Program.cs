7123417472992c0cc59950f628c63160e99333ac
Aleksandr Maha <maha.aleksandr@apriorit.com>
2017-06-14 17:12:08 +0300

    add linq query for task 14

--- EFLinqSqlTask/EFLinqSqlTask/Program.cs
+++ EFLinqSqlTask/EFLinqSqlTask/Program.cs
@@ -1,20 +1,40 @@
 ﻿namespace EFLinqSqlTask
 {
     using System;
+    using System.Linq;
 
     class Program
     {
         static void Main(string[] args)
         {
-            using (var ctx = new Computer())
+            using (var db = new Computer())
             {
-                foreach (var product in ctx.Products)
+                var res14 = db.Products.Where(products => db.Products.GroupBy(products0 => new
                 {
-                    Console.WriteLine(product.maker);
-                }
-            }
+                    products0.maker,
+                    products0.type
+                }).Select(g => new
+                {
+                    g.Key.maker,
+                    g.Key.type
+                }).GroupBy(x => new
+                {
+                    x.maker
+                }).Where(g => g.Count() == 1).Select(g => new
+                {
+                    g.Key.maker
+                }).Contains(new {products.maker })).GroupBy(products => new
+                {
+                    products.maker,
+                    products.type
+                }).Where(g => g.Count() > 1).Select(g => new
+                {
+                    g.Key.maker,
+                    g.Key.type
+                });
 
-            Console.ReadLine();
+                Console.ReadLine();
+            }
         }
     }
 }
