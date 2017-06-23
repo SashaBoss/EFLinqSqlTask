32b0a9261b9a56d750a418449b3fc3f531c03267
Aleksandr Maha <maha.aleksandr@apriorit.com>
2017-06-23 16:19:02 +0300

    add LINQ queries for tasks

--- EFLinqSqlTask/EFLinqSqlTask/Program.cs
+++ EFLinqSqlTask/EFLinqSqlTask/Program.cs
@@ -9,10 +9,10 @@
         {
             using (var db = new Computer())
             {
-                var res14 = db.Products.Where(products => db.Products.GroupBy(products0 => new
+                var res14 = db.Products.Where(product => db.Products.GroupBy(p => new
                 {
-                    products0.maker,
-                    products0.type
+                    p.maker,
+                    p.type
                 }).Select(g => new
                 {
                     g.Key.maker,
@@ -23,7 +23,7 @@
                 }).Where(g => g.Count() == 1).Select(g => new
                 {
                     g.Key.maker
-                }).Contains(new {products.maker })).GroupBy(products => new
+                }).Contains(new { product.maker })).GroupBy(products => new
                 {
                     products.maker,
                     products.type
@@ -32,6 +32,63 @@
                     g.Key.maker,
                     g.Key.type
                 });
+
+                var res17 = db.Laptops.Where(l => l.speed < db.PCs.Select(pc => new { pc.speed })
+                                      .Min(p => p.speed))
+                                      .Select(l => new
+                                      {
+                                          l.Product.type,
+                                          l.model,
+                                          l.speed
+                                      }).Distinct();
+
+                var res23 = db.PCs.Where(pc => pc.speed >= 750 && db.Laptops.Where(laptop => laptop.speed >= 750)
+                                  .Select(laptop => new
+                                  {
+                                      laptop.Product.maker
+                                  })
+                                  .Contains(new { pc.Product.maker }))
+                                  .Select(pc => new
+                                  {
+                                      pc.Product.maker
+                                  }).Distinct();
+
+                var res24 = db.PCs.Select(pc => new
+                {
+                    pc.model,
+                    pc.price
+                })
+                .Union(db.Laptops.Select(laptop => new
+                {
+                    laptop.model,
+                    laptop.price
+                }))
+                .Union(db.Printers.Select(printer => new
+                {
+                    printer.model,
+                    printer.price
+                }))
+                    .Where(product => product.price == db.PCs.Select(pc => new
+                    {
+                        pc.price
+                    })
+                    .Union(db.Laptops.Select(laptop => new
+                    {
+                        laptop.price
+                    }))
+                    .Union(db.Printers.Select(printer => new
+                    {
+                        printer.price
+                    }))
+                    .Select(p => new
+                    {
+                        p.price
+                    }).Max(p => p.price)).Select(p => new
+                    {
+                        p.model
+                    });
+
+                Console.WriteLine();
 
                 Console.ReadLine();
             }
